cba55c24463cc3e190101a8bc64b5f4908c47fd7
k0xzy <koxzy22@gmail.com>
2022-07-27 13:47:14 +0300

    filmsManager

---
+++ src/test/java/FilmManagerTest.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+public class FilmManagerTest {
+
+
+    @Test
+    public void findAllFilm() {
+        FilmsManager manager = new FilmsManager();
+        manager.addFilm("film1");
+        manager.addFilm("film2");
+        manager.addFilm("film3");
+        manager.addFilm("film4");
+        manager.addFilm("film5");
+        manager.addFilm("film6");
+        manager.addFilm("film7");
+        manager.addFilm("film8");
+        manager.addFilm("film9");
+        manager.addFilm("film10");
+        String[] expected = {"film1", "film2", "film3", "film4", "film5", "film6", "film7", "film8", "film9", "film10"};
+        String[] actual = manager.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLast() {
+        FilmsManager manager = new FilmsManager(5);
+        manager.addFilm("film1");
+        manager.addFilm("film2");
+        manager.addFilm("film3");
+        manager.addFilm("film4");
+        manager.addFilm("film5");
+        manager.addFilm("film6");
+        manager.addFilm("film7");
+        manager.addFilm("film8");
+        manager.addFilm("film9");
+        manager.addFilm("film10");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"film10", "film9", "film8", "film7", "film6"};
+
+        assertArrayEquals(expected, actual);
+    }
+
+}
