695a225fcf71597ae7088460be0195f41db60573
k0xzy <koxzy22@gmail.com>
2022-08-09 13:23:21 +0300

    Updt2_0

--- src/test/java/FilmManagerTest.java
+++ src/test/java/FilmManagerTest.java
@@ -25,8 +25,8 @@ public class FilmManagerTest {
     }
 
     @Test
-    public void findLast() {
-        FilmsManager manager = new FilmsManager(5);
+    public void findLastLimNorm() {
+        FilmsManager manager = new FilmsManager(10);
         manager.addFilm("film1");
         manager.addFilm("film2");
         manager.addFilm("film3");
@@ -39,7 +39,43 @@ public class FilmManagerTest {
         manager.addFilm("film10");
 
         String[] actual = manager.findLast();
-        String[] expected = {"film10", "film9", "film8", "film7", "film6"};
+        String[] expected = {"film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2", "film1"};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastLimMax() {
+        FilmsManager manager = new FilmsManager(10);
+        manager.addFilm("film1");
+        manager.addFilm("film2");
+        manager.addFilm("film3");
+        manager.addFilm("film4");
+        manager.addFilm("film5");
+        manager.addFilm("film6");
+        manager.addFilm("film7");
+        manager.addFilm("film8");
+        manager.addFilm("film9");
+        manager.addFilm("film10");
+        manager.addFilm("film11");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"film11", "film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2"};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastLimMin() {
+        FilmsManager manager = new FilmsManager(10);
+        manager.addFilm("film1");
+        manager.addFilm("film2");
+        manager.addFilm("film3");
+        manager.addFilm("film4");
+        manager.addFilm("film5");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"film5", "film4", "film3", "film2", "film1"};
 
         assertArrayEquals(expected, actual);
     }
